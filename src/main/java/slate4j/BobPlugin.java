34aec93e3d9054f0ec1f307dbe462fe8f5bceb11
Jurgen Voorneveld <jurgen@ngti.nl>
2020-02-10 17:34:26 +0100

    Updated bobfile, renamed plugin class

--- src/main/java/slate4j/BobPlugin.java
+++ src/main/java/slate4j/BobPlugin.java
@@ -1,16 +1,17 @@
-package bobslate4j;
+package slate4j;
 
 import bobthebuildtool.pojos.buildfile.Project;
 
 import java.util.Map;
 
-public enum Main {;
+public enum BobPlugin {;
 
     public static void installPlugin(final Project project) {
-        project.addCommand("slate4j", "Generates documentation in slatedocs format", Main::generateSlateDocs);
+        project.addCommand("slate4j", "Generates documentation in slatedocs format", BobPlugin::generateSlateDocs);
     }
 
     private static int generateSlateDocs(final Project project, final Map<String, String> env, final String[] args) {
         return 0;
     }
+
 }
